0c9ba3d7b9ba8cd34c54694206f61e96f37b890a
aash1999 <aakashsingh717@gmail.com>
2020-12-23 12:06:36 +0530

    navbar account icon

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,38 +3,45 @@ import logo from "../../assets/img/dsc_iiitk_logo_white.png";
 import ham from "../../assets/img/hamburger.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./navbar.css";
-import Typist from 'react-typist';
+import account from "../../assets/img/account1.svg";
+import Typist from "react-typist";
 const Example = (props) => {
+  var signedIn = true;
+  function signedInorNot() {
+    if (signedIn === true) {
+      return (
 
+
+        <div className="accountIcon ">
+          <img className="accountImg" src={account} />
+        </div>
+
+      );
+    } else {
+      return (
+
+          <div className="signin">SignIn</div>
+
+      );
+    }
+  }
 
   function CollapseElement() {
-    if (window.screen.width >= 959 ) {
+    if (window.screen.width >= 959) {
       return (
         <div className="links-container ">
-          <div className="link">
-            Home
-          </div>
-          <div className="link">
-            AboutUs
-          </div>
-          <div className="link">
-            Events
-          </div>
-          <div className="link">
-            Projects
-          </div>
-          <div className="link">
-            Team
-          </div>
-          <div className="signin">
-            SignIn
-          </div>
+          <div className="link">Home</div>
+          <div className="link">AboutUs</div>
+          <div className="link">Events</div>
+          <div className="link">Projects</div>
+          <div className="link">Team</div>
+          {signedInorNot()}
         </div>
       );
     } else {
       return (
         <div className="links-container">
-            <img className="ham" src={ham} />
+          <img className="ham" src={ham} />
         </div>
       );
     }
